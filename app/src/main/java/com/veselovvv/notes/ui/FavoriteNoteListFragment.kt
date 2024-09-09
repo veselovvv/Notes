ad346d59fe3e55b315683ef138e082840feed2e7
veselovvv <vycheslav5veselov@gmail.com>
2024-09-09 09:22:06 +0300

    Updated the project

--- app/src/main/java/com/veselovvv/notes/ui/FavoriteNoteListFragment.kt
+++ app/src/main/java/com/veselovvv/notes/ui/FavoriteNoteListFragment.kt
@@ -2,13 +2,8 @@ package com.veselovvv.notes.ui
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.ViewModelProviders
 
 class FavoriteNoteListFragment : NoteListFragment() {
-    private val notesViewModel: NotesViewModel by lazy {
-        ViewModelProviders.of(this).get(NotesViewModel::class.java)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
